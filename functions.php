84c14dc11a86d8529f67d0193227d82dc72fd18d
jsuch11 <jysuchet@gmail.com>
2022-12-05 10:26:17 +0100

    Removed function to remove comments

--- functions.php
+++ functions.php
@@ -88,47 +88,6 @@ function editing_elementor_library_capability($args, $post_type)
 }
 add_filter('register_post_type_args', 'editing_elementor_library_capability', 10, 2);
 
-
-add_action('admin_init', function () {
-    // Redirect any user trying to access comments page
-    global $pagenow;
-     
-    if ($pagenow === 'edit-comments.php') {
-        wp_safe_redirect(admin_url());
-        exit;
-    }
- 
-    // Remove comments metabox from dashboard
-    remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
- 
-    // Disable support for comments and trackbacks in post types
-    foreach (get_post_types() as $post_type) {
-        if (post_type_supports($post_type, 'comments')) {
-            remove_post_type_support($post_type, 'comments');
-            remove_post_type_support($post_type, 'trackbacks');
-        }
-    }
-});
- 
-// Close comments on the front-end
-add_filter('comments_open', '__return_false', 20, 2);
-add_filter('pings_open', '__return_false', 20, 2);
- 
-// Hide existing comments
-add_filter('comments_array', '__return_empty_array', 10, 2);
- 
-// Remove comments page in menu
-add_action('admin_menu', function () {
-    remove_menu_page('edit-comments.php');
-});
- 
-// Remove comments links from admin bar
-add_action('init', function () {
-    if (is_admin_bar_showing()) {
-        remove_action('admin_bar_menu', 'wp_admin_bar_comments_menu', 60);
-    }
-});
-
 // Remove RSS feed from WordPress
 function itsme_disable_feed() {
  wp_die( __( 'No feed available, please visit the <a href="'. esc_url( home_url( '/' ) ) .'">homepage</a>!' ) );
